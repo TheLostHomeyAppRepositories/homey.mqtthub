b5ad278e08b92114924a0977a749578da4f492e5
RonnyWinkler <ronny.winkler@web.de>
2025-02-14 19:58:35 +0100

    4.3.11
    Changed capability selection in pair view. Added capability id.

--- assets/utils.js
+++ assets/utils.js
@@ -4,3 +4,7 @@ function sortByTitle(a, b, lang) {
     let title2 = b.title[lang].trim().toLowerCase();
     return title1 < title2 ? -1 : title1 > title2 ? 1 : 0;
 }
+
+function sortById(a, b, lang) {
+    return a.id < b.id ? -1 : a.id > b.id ? 1 : 0;
+}
